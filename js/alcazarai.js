cd77c721d85c95ebb8c39ea10310939cb533d2d4
Thomas Chapeaux <chapeauxthomas@gmail.com>
2015-01-17 14:32:39 +0100

    Add preventLoop logic to Solver

--- js/alcazarai.js
+++ js/alcazarai.js
@@ -8,9 +8,11 @@ var AlcazarAI = function(level) {
 AlcazarAI.prototype.solve = function() {
     var wallUnstable = true;
     var pathUnstable = true;
-    while (wallUnstable || pathUnstable) {
+    var loopUnstable = true;
+    while (wallUnstable || pathUnstable || loopUnstable) {
         wallUnstable = this.fillObviousWalls();
         pathUnstable = this.fillObviousPaths();
+        loopUnstable = this.preventLoops();
     }
 };
 
@@ -63,4 +65,80 @@ AlcazarAI.prototype.fillObviousPaths = function() {
         }
     }
     return foundObviousPath;
+};
+
+AlcazarAI.prototype.preventLoops = function() {
+    // Prevent partial paths to form a loop, i.e:
+    // if both ends are adjacent, add a wall between them
+    // if both ends are one tile apart, prevent the separation tile to connect them (if possible)
+    var foundPreventableLoop = false;
+
+    var i, j;
+
+    // create marker array with the same size as the level
+    var marks = [];
+    for (i = 0; i < this.level.grid.sizeX; i++) {
+        marks.push([]);
+        for (j = 0; j < this.level.grid.sizeY; j++) {
+            marks[i].push(false);
+        }
+    }
+    marks[-1] = []; marks[-1][-1] = false; // outerTile
+
+    // look for partial paths endpoints (tile with exactly one path link)
+    for (i = this.level.grid.sizeX - 1; i >= 0; i--) {
+        for (j = this.level.grid.sizeY - 1; j >= 0; j--) {
+            var firstTile = this.level.grid.getTile(i, j);
+            var firstPaths = firstTile.getNeighborPaths();
+            if (firstPaths.length == 1 && marks[i][j] === false) {
+                // path endpoint detected
+
+                // find other endpoint
+                var currentLink = firstPaths[0];
+                var currentTile = currentLink.other(firstTile);
+                while (currentTile.getNeighborPaths().length == 2) {
+                    var paths = currentTile.getNeighborPaths();
+                    currentLink = (paths[0] === currentLink) ? paths[1] : paths[0];
+                    currentTile = currentLink.other(currentTile);
+                }
+                var lastTile = currentTile;
+
+                // mark both endpoints
+                marks[firstTile.x][firstTile.y] = true;
+                marks[lastTile.x][lastTile.y] = true;
+
+                // compare endpoints position
+                var dx = lastTile.x - firstTile.x;
+                var dy = lastTile.y - firstTile.y;
+
+                // handle adjacent endpoints
+                var separationLink;
+                if (dy === 0) {
+                    if (dx == 1) {
+                        separationLink = firstTile.neighborLinks[Tile.directions.RIGHT];
+                    } else if (dx == -1) {
+                        separationLink = firstTile.neighborLinks[Tile.directions.LEFT];
+                    }
+                } else if (dx === 0) {
+                    if (dy == 1) {
+                        separationLink = firstTile.neighborLinks[Tile.directions.DOWN];
+                    } else if (dy == -1) {
+                        separationLink = firstTile.neighborLinks[Tile.directions.UP];
+                    }
+                }
+                if (separationLink && separationLink.state != TileLink.stateEnum.LEVEL_WALL) {
+                    if (separationLink.state != TileLink.stateEnum.USER_WALL) {
+                        foundPreventableLoop = true;
+                        separationLink.state = TileLink.stateEnum.USER_WALL;
+                    }
+                }
+
+                // handle bouncing (endpoints separated by one tile)
+                // TODOOOO...
+
+            }
+        }
+    }
+
+    return foundPreventableLoop;
 };
